9a0fdb65650fa796fbc3889ad61086975cde86a9
lobthere <lobthere@protonmail.com>
2025-11-20 15:24:03 +0100

    added recurcive function

--- projet-leopold-djibril-lucien/scripts/json.js
+++ projet-leopold-djibril-lucien/scripts/json.js
@@ -41,76 +41,100 @@ async function loadJsonFile() {
     const response = await fetch('projet-leopold-djibril-lucien/json/map.json');
     const data = await response.json();
 
-    searchInJson(data['secteur'][0], 'DEV FRONT END', []);
-
-    //data['secteur'].forEach(element => {
-    //    searchInJson(element, 'DEV FRONT END', [], 0);
-    //});
+    data['secteur'].forEach(element => {
+        searchInJson(element, 'DEV FRONT END', [], 0);
+    });
 }
 
 
-function searchInJson(jsonInput, attendu, currentPath){
+function searchInJson(jsonInput, attendu){
     /*
         depth search in the json
             jsonInput: dict -> the dict that hold the card
             attendu: str -> the expected value for the name
-            currentPath: list -> hold the current path in the json file of the card, ex : ["secteur", 0, 0, 0, 0, 3]; between every number is expected a "sub" for the real path
     */
-    let cardIdentity = jsonInput['card-identity']; //get the name value from the array
-    let cardName = jsonInput['name']; //get the card name
+    //let cardIdentity = jsonInput['card-identity']; //get the name value from the array
+    //let cardName = jsonInput['name']; //get the card name
+    //
+    //let toSend = [];
+    //
+    ///*___ Test some valuse ___*/
+    //console.log("---------------------------------------")
+    //console.log(cardName);
+    ////console.log(cardIdentity);
+    //console.log(jsonInput);
+    ////console.log(attendu);
+    ////console.log(currentPath);
+    ////console.log(position);
+    ////console.log("_____________________________");
+    ////console.log(jsonInput['sub'][position]);
+    ////console.log("_____________________________")
+    ////console.log("NEXT VALUE \n \n \n")
+    //console.log("---------------------------------------")
+    //
+    //
+    //
+    ////check if the card has a sub categorie, but checking it s card identity
+    //if (cardIdentity === "vide lower"){
+    //    //tell if the card is the right one
+    //    if (cardName.includes(attendu)){
+    //        toSend.push([attendu, jsonInput['card-identity'], currentPath, jsonInput['description']]); //add all the element to make the card in the toSend list
+    //    }
+    //    //check if the next value is in the array
+    //    else if (position + 1 == jsonInput.length) {
+    //        currentPath.pop(); //remove the last element from the path 
+    //        position = 0
+    //        searchInJson(jsonInput, attendu, currentPath, position);
+    //    }
+    //    else{
+    //        currentPath[currentPath.length - 1] = position; //edit the position in the path
+    //        searchInJson(jsonInput, attendu, currentPath, position + 1); //search the next value in 
+    //    }
+    //}
+    //
+    //
+    ////do the cards who have a sub categorie in them
+    //else{
+    //    //if it s one of the cards
+    //    if (cardName.includes(attendu)){
+    //        toSend.push([attendu, jsonInput['card-identity'], currentPath]); //return all the element to make the card
+    //    };
+    //    
+    //    // if the next card is the last one 
+    //    if (currentPath[currentPath.length - 1 ] + 1 == jsonInput.length) {
+    //        currentPath.pop();
+    //        currentPath[currentPath.length - 2] += 1;
+    //        position = 0;
+    //        searchInJson(jsonInput, attendu, currentPath, position);
+    //    }
+    //    else {
+    //        searchInJson(jsonInput['sub'][position], attendu, currentPath, position + 1);
+    //    };
+    //
+    //    return toSend;
+    //}
+    //
+    //return toSend
+    let toR = [];
 
-    let position = 
-
-    /*___ Test some valuse ___*/
-    console.log(cardName);
-    console.log(cardIdentity);
-    console.log("---------------------------------------")
-    console.log(jsonInput);
-    console.log(attendu);
-    console.log(currentPath);
-    console.log(position);
-    console.log("_____________________________");
-    console.log(jsonInput['sub'][position]);
-    console.log("_____________________________")
-    console.log("NEXT VALUE \n \n \n")
-    
-    
-
-    //check if the card has a sub categorie, but checking it s card identity
-    if (cardIdentity === "vide lower"){
-        //tell if the card is the right one
-        if (cardName === attendu){
-            console.log("hello");
-            return [attendu, jsonInput['card-identity'], currentPath, jsonInput['description']]; //return all the element to make the card
-        }
-        //check if the next value is in the array
-        else if (position == jsonInput.length()) {
-            currentPath.pop(); //remove the last element from the path 
-            currentPath[currentPath.length() - 2] += 1; // add one to the previous 
-            searchInJson(jsonInput, attendu, currentPath, position);
-        }
-        else if (position === 0){
-            currentPath.push(position);
-        }
-        else{
-            currentPath[currentPath.length() - 1] = position; //edit the position in the path
-            searchInJson(jsonInput, attendu, currentPath, position + 1); //search the next value in 
-        }
+    if (jsonInput['card-identity'] !== 'vide lower'){ //if card not the last row
+        jsonInput['sub'].forEach(element => { //go into each list element
+            console.assert()
+            toR.push(jsonInput);
+            searchInJson(element, attendu); //search the lower element
+        });
+    }
+    else {
+        toR.push(jsonInput);
     }
 
-    //do the cards who have a sub categorie in them
-    else{
-        if (cardName === attendu){
-            console.log("hello");
-            return [attendu, jsonInput['card-identity'], currentPath]; //return all the element to make the card
-        } else if (0 === 0) {
-            console.log("hello world!")
-        }
-    }
+    console.log(toR);
 }
 
-const jsonFile = loadJsonFile();
+loadJsonFile();
 
-console.log(jsonFile);
+console.log(jsonFile());
+
+
 //cardCreator(data['secteur'][0]['name'], 'tech-list', 'description', data['secteur'][0]['card-identity']);
 //console.log(data['secteur'][0]['name'])
