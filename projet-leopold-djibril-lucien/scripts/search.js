b3c21c4736c9957e9df40e5f582840aa2e594f1b
lobthere <lobthere@protonmail.com>
2025-11-21 00:39:49 +0100

    made the search bar working, got ride of some code in the search bar, added the description in the json file

--- projet-leopold-djibril-lucien/scripts/search.js
+++ projet-leopold-djibril-lucien/scripts/search.js
@@ -1,21 +1,16 @@
-const file = "projet-leopold-djibril-lucien/json/map.json"
+const file = "projet-leopold-djibril-lucien/json/map.json" //the file path
 const form = document.querySelector("#cherch").value;
 const btn = document.querySelector("#search");
 const carte = document.querySelector(".cart");
 
 function validsearchbar(event){
     event.preventDefault()
-    const rechercher = document.querySelector("#recherche").value;
-    fetch(file)
-    .then((techno)=>{  const BoxName = document.querySelector(".pretitre").value})
-    if (recherche!==""){
-        carte = carte.filter((recherche) => carte.toLowerCase().includes(input.toLowerCase()))
-      }
-       carte.forEach((carte) => {
-    
-        list.insertAdjacentHTML("beforeend", card);
-      })
+    const recherche = document.querySelector("#recherche").value;
+    if (recherche !== ""){
+        console.log(loadJsonFile(recherche, 'artcart'));
+    };
 }
-  
-console.log(btn)
+
 btn.addEventListener("click", validsearchbar); //on ecoute quand quelqu'un click sur le bouton
+
+
