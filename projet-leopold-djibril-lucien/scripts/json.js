7ec3acbb27db8028926c8b02700c640f73c6787e
lobthere <lobthere@protonmail.com>
2025-11-22 17:51:01 +0100

    enleve les doublons et affiche tout les parents d un doublons

--- projet-leopold-djibril-lucien/scripts/json.js
+++ projet-leopold-djibril-lucien/scripts/json.js
@@ -45,11 +45,11 @@ async function loadJsonFile(textToSearch, subCard) {
     const response = await fetch('projet-leopold-djibril-lucien/json/map.json');
     const data = await response.json();
 
-    toR = []; // 0 -> name; 1 -> card-identity; 2 -> parents; 3 -> Description;
+    let toR = []; // 0 -> name; 1 -> card-identity; 2 -> parents; 3 -> Description;
     data['secteur'].forEach(element => {
         searchInJson(element, textToSearch.toLowerCase());
     });
-    temp = document.getElementById(subCard);""
+    const temp = document.getElementById(subCard);
     temp.innerHTML = '';
     toR.forEach(element => {
         cardCreator(element[0], subCard, element[3], element[1]);
@@ -67,11 +67,15 @@ function searchInJson(jsonInput, attendu, previous){
     if (jsonInput['card-identity'] !== 'vide-lower'){ //if card not the last row
         text = jsonInput['name'].toLowerCase(); //put text in lowercase
         if (text.includes(attendu)){ //check if the attendue value is in the name 
-            try {
-                toR.push([jsonInput['name'], jsonInput['card-identity'], previous, jsonInput['description']]) //add it to the toR list
-            }
-            catch{
-                toR.push([jsonInput['name'], jsonInput['card-identity'], previous, 'pas de description pour l instant']) //add it to the toR list
+            if (!(toR.includes(attendu))){
+                try {
+                    toR.push([jsonInput['name'], jsonInput['card-identity'], [previous], jsonInput['description']]); //add it to the toR list
+                }
+                catch{
+                    toR.push([jsonInput['name'], jsonInput['card-identity'], previous, 'pas de description pour l instant']); //add it to the toR list
+                }
+            }else{
+                toR[/* La position a laquelle se trouve notre element */][2].push(previous);
             }
         }
         jsonInput['sub'].forEach(element => { //go into each list element
@@ -81,7 +85,7 @@ function searchInJson(jsonInput, attendu, previous){
     else {
         text = jsonInput['name'].toLowerCase(); //put the text in lowercase
         if (text.includes(attendu)){ //check if the attendue value is in the name 
-            toR.push([jsonInput['name'], jsonInput['card-identity'],previous, jsonInput['description']]) //add it to the toR list
+            toR.push([jsonInput['name'], jsonInput['card-identity'],[previous], jsonInput['description']]) //add it to the toR list
         }
     }
 }
