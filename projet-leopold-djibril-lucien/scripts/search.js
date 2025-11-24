009a1760efa43b8f60d3f91aee8338231387ae9d
Djibril-Amor Garbaa <djibril.garbaa@proton.me>
2025-11-24 13:25:31 +0100

    modif mineurs pour que ça fonctionne

--- projet-leopold-djibril-lucien/scripts/search.js
+++ projet-leopold-djibril-lucien/scripts/search.js
@@ -2,21 +2,20 @@ const file = "projet-leopold-djibril-lucien/json/map.json" //the file path
 const form = document.querySelector("#cherch").value;
 const btn = document.querySelector("#search");
 const carte = document.querySelector(".cart");
-const read = document.querySelector("#recherche")
 
 function validsearchbar(event){
     event.preventDefault()
     const recherche = document.querySelector("#recherche").value;
     if (recherche !== ""){
+        console.log(loadJsonFile(recherche, 'artcart'));
     };
 }
 
 function search(){
     const searchbar = document.querySelector("#recherche");
     searchbar.addEventListener("keyup", function (evt) {
-        something(this.value)});
+        something(this.value)});    
 }
 
 btn.addEventListener("click", validsearchbar); //on ecoute quand quelqu'un click sur le bouton
-read.addEventListener("keyup",validsearchbar)
-
+searchbar.addEventListener("keyup",validsearchbar);
